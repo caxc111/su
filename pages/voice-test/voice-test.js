965ee3dd622b1ef81caa2b233fd29cf293e1a412
caxc111 <344080115qq@gmail.com>
2025-04-09 12:44:17 +0800

    修复语音测试

--- pages/voice-test/voice-test.js
+++ pages/voice-test/voice-test.js
@@ -12,57 +12,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    console.log('pages/voice-test/voice-test: onLoad have been invoked')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
+    console.log('pages/voice-test/voice-test: onReady have been invoked')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    console.log('pages/voice-test/voice-test: onShow have been invoked')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide() {
-
+    console.log('pages/voice-test/voice-test: onHide have been invoked')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-
+    console.log('pages/voice-test/voice-test: onUnload have been invoked')
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    console.log('pages/voice-test/voice-test: onPullDownRefresh have been invoked')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
+    console.log('pages/voice-test/voice-test: onReachBottom have been invoked')
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage() {
-
+    console.log('pages/voice-test/voice-test: onShareAppMessage have been invoked')
   }
 }) 
- 
- 
